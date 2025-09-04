36e09259e74976f144e5a3aa42e9a900033b511d
alanwarthon <familiawarthon@gmail.com>
2025-09-03 19:44:55 -0700

    Practicando codigo de memoria

---
+++ script01.js
@@ -0,0 +1,8 @@
+// Variable para capturar el boton
+const btn01 = document.getElementById("btnCalculate");
+btn01.addEventListener("click", calculateGrades);
+
+
+function calculateGrades(){
+  console.log("Boton clickeado!");
+}
