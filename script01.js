dd40d0b56d24eaa6f55068436bd5f762df44a119
alanwarthon <familiawarthon@gmail.com>
2025-09-13 16:29:30 -0700

    Second part done

--- script01.js
+++ script01.js
@@ -2,7 +2,28 @@
 const btn01 = document.getElementById("btnCalculate");
 btn01.addEventListener("click", calculateGrades);
 
+const usernameField = document.getElementById("txt01");
+const grade01Field = document.getElementById("txt02");
+const grade02Field = document.getElementById("txt03");
+const grade03Field = document.getElementById("txt04");
+
+const txtMessageField = document.getElementById("txtS");
+
+
 
 function calculateGrades(){
   console.log("Boton clickeado!");
+  let username = usernameField.value;
+  let grade01 = grade01Field.value;
+  let grade02 = grade02Field.value;
+  let grade03 = grade03Field.value;
+
+  let average = Number(grade01)+Number(grade02)+Number(grade03);
+  average = average/3;
+
+  console.log(`Welcome ${username}`);
+  console.log(`Your final grade is ${average}`);
+
+  txtMessageField.textContent = `Welcome ${username} \nYour final grade is ${average}`;
+
 }
